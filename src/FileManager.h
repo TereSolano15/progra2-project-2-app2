2e7f6ed4451a6458544c70ef549327a23f78d117
fabio0610 <fabio0610@hotmail.com>
2020-11-05 12:18:39 -0600

    creacion de otras clases

---
+++ src/FileManager.h
@@ -0,0 +1,14 @@
+//
+// Created by Fabio Villalobos on 5/11/2020.
+//
+
+#ifndef MY_PROJECT_NAME_FILEMANAGER_H
+#define MY_PROJECT_NAME_FILEMANAGER_H
+
+
+class FileManager {
+
+};
+
+
+#endif //MY_PROJECT_NAME_FILEMANAGER_H
