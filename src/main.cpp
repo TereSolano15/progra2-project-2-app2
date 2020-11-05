145383ae06f36b3683e747f793833646a7bb44f5
fabio0610 <fabio0610@hotmail.com>
2020-11-05 12:17:30 -0600

    pequeños cambios

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,15 @@
 // Created by Tere Solano on 27/10/2020.
 //
 #include <iostream>
+#include "Menu.h"
 int main(){
+
+    Menu* menu = new Menu();
+
+    menu->MenuPrincipal();
+
+    delete menu;
+
     return 0;
+
 }
