105b730104513b72474a8dd2f10c8873c78551ad
fabio0610 <fabio0610@hotmail.com>
2020-11-05 20:04:36 -0600

    implementacion de metodos

--- src/FileManager.cpp
+++ src/FileManager.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "FileManager.h"
+
+void FileManager::saveBinary() {
+
+    ofstream  archivo;
+
+    try{
+
+        archivo.open("ArchivoBinario.dat", ios::app | ios::binary);
+
+    }
+
+    catch (std::ifstream::failure a) {
+
+        throw runtime_error("Error abriendo el archivo");
+
+    }
+
+   // archivo.write((char*) &persons, sizeof(Person));
+
+    archivo.close();
+
+}
--- src/FileManager.h
+++ src/FileManager.h
@@ -4,10 +4,15 @@
 
 #ifndef MY_PROJECT_NAME_FILEMANAGER_H
 #define MY_PROJECT_NAME_FILEMANAGER_H
-
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+using namespace std;
 
 class FileManager {
-
+public:
+    void saveBinary();
 };
 
 
