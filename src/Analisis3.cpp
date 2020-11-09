0dab639739b14126b5a43841f0e5e35cb8eaced5
fabio0610 <fabio0610@hotmail.com>
2020-11-08 18:28:33 -0600

    cambios menores

---
+++ src/Analisis3.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Fabio Villalobos on 8/11/2020.
+//
+
+#include "Analisis3.h"
---
+++ src/Analisis3.h
@@ -0,0 +1,14 @@
+//
+// Created by Fabio Villalobos on 8/11/2020.
+//
+
+#ifndef MY_PROJECT_NAME_ANALISIS3_H
+#define MY_PROJECT_NAME_ANALISIS3_H
+
+
+class Analisis3 {
+
+};
+
+
+#endif //MY_PROJECT_NAME_ANALISIS3_H
